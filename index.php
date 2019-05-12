1bf4cbaad52c63d8bef8ade58021b0ffd0b9b4b7
Reign <reignzj.westry@gmail.com>
2019-05-11 22:17:22 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+//connects db
+include_once'includes/dbh/dbh.inc.php';
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>POPQUIZ</title>
+    <!-- DEFAULT STYLESHEET -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<!--
+    TODO
+        1. Connect Database
+        2. Build the Login System
+-->
+    <head>
+        <div>
+            <p>POPQUIZ</p>
+
+        </div>
+        <nav>
+            <!-- Main Navigation -->
+            <ul>
+                <li>Home</li>
+                <li>Decks</li>
+                <li></li>
+                <li></li>
+                <li>Challenges</li>
+            </ul>
+            <br>
+            <!-- LOGIN -->
+            <ul>
+                <li>Logout</li>
+                <li>Login</li>
+            </ul>
+        </nav>
+
+
+    </head>
+
+</body>
+</html>
---
+++ objective.md
@@ -0,0 +1,33 @@
+# **POPQUIZ** (PHP Project #1) 
+    
+### Objectives
+    
+    * POPQUIZ - a personal quizzing application to help people study and memorize data that they 
+    are studying.
+    
+    1. CREATE A WEBSITE, Android, iOS APP that syncs together
+    2. CREATE a logo for POPQUIZ
+    3. SHARE QUIZES or scores with others
+    4. Competitions based on type or category
+    
+#### INPUT FEATURES
+    1. ADD & RECORD voice recordings
+    2. ADD images
+    3. ADD text
+    4. ADD videos (SHORT CLIPS)
+    
+##### NOTIFICATION FEATURES
+    1. IN-DRAWER POPUP QUIZES
+    2. TIMED POPQUIZ NOTIFICATIONS with INPUT (LIKE txt message reply like TEXTRA APP)
+    3. QUESTION RATING SYSTEM
+    4. 
+     
+     
+#####
+     
+###### VERSION
+    1.0
+    
+     
+###### AUTHOR
+    <a href="www.reignwestry.com">Reign Westry</a>
